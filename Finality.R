dbd0638b8dfdfed414ced35323082c8c2000fc29
Erik Vandenkooy <154548507+evandenkooy@users.noreply.github.com>
2024-01-19 19:05:57 -0500

    Update Finality.R
    heatmap and animation

--- Finality.R
+++ Finality.R
@@ -1,4 +1,3 @@
-#loading packages
 library('tidyverse')
 library('ggplot2')
 library('shiny')
@@ -11,7 +10,6 @@ library('ggthemes')
 library('readxl')
 library("gifski")
 library("png")   
-
 
 #reading in data
 df <- read_csv('NBA_2023_SHOTS.csv')
@@ -71,7 +69,7 @@ for (name in unique(players$PLAYER_NAME)){
   }
 }
 
-# #
+#
 # #
 # #
 # ##
@@ -121,7 +119,7 @@ for (name in unique(players$PLAYER_NAME)){
 # shinyApp(ui, server)
 # #
 # #
-# #
+#
 
 pts <- players %>% 
   select(PLAYER_NAME) %>% 
@@ -136,7 +134,7 @@ for (row in 1:nrow(df)){
   } 
 }
 
-pts <- subset(pts, Three_Pts_Made >= 200)
+pts <- subset(pts, Three_Pts_Made >= 213)
 
 df$GAME_DATE = as.Date(df$GAME_DATE, format = "%m-%d-%Y")
 
@@ -156,38 +154,75 @@ dateCondensed <- withDate %>%
   filter(SHOT_MADE==TRUE)
 
 
-##########################
+# ##########################
+# 
+# 
+# 
+# 
+#   
+#   plot<-dateCondensed%>% 
+#     ggplot(aes(x=GAME_DATE,y=Three_Pts_Made_Total,color=PLAYER_NAME))+
+#     geom_line(alpha=0.8)+
+#     theme_solarized_2(light=F)+
+#     labs(title="NBA Total Three Pointers Race, 2022-2023 Season",
+#          caption="Source: Dom Samangy, Github, NBA_Shots_04_23")+
+#     theme(text=element_text(colour="#EEEEEE"),
+#           title=element_text(colour="#EEEEEE",size=9,face = "bold"),
+#           plot.title=element_text(hjust=0.5),
+#           axis.title.x = element_blank(),
+#           panel.grid.minor.x=element_blank(),
+#           legend.background = element_blank(),
+#           legend.key= element_blank(),
+#           legend.position=c(0.095, 0.7), ## legend at top-left, inside the plot
+#           plot.margin = unit(c(0.5,1.3,0.5,0.5), "cm"))+ 
+#     scale_x_date(date_labels = "%b",date_breaks ="1 month")
+#   
+#   
+#   plot.animation=plot+
+#     transition_reveal(GAME_DATE)+
+#     view_follow(fixed_y=T)
+#   
+#   #Save animation
+#   animate(plot.animation, height=365,width=608,fps=30,duration=10, renderer = gifski_renderer())
+#   
+#   anim_save(plot.animation, filename = "NBA_Total_Three_Pointers_Race_2022-2023_Season.gif")
+#   
+#   
+#   
+#   
+#   ####################
 
+ui <- fluidPage(
+  titlePanel("NBA Shot Heatmap"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("player", "Select Player", choices = unique(df$PLAYER_NAME))
+    ),
+    mainPanel(
+      plotOutput("heatmapPlot")
+    )
+  )
+)
 
+server <- function(input, output) {
+  output$heatmapPlot <- renderPlot({
+    # Filter data for the selected player and shots where LOC_Y is less than or equal to 40
+    player_data <- filter(df, PLAYER_NAME == input$player, LOC_Y <= 40)
+    
+    # Calculate opacity based on shot frequency
+    opacity <- scale(player_data$LOC_Y)
+    
+    # Create a ggplot heatmap with variable opacity and color scale
+    ggplot(player_data, aes(x = LOC_X, y = LOC_Y, fill = ..level.., alpha = ..level..)) +
+      stat_density_2d(geom = "polygon", color = "white") +
+      scale_fill_gradient(low = "green", high = "red", breaks = seq(0, 1, 0.1), guide = "legend") +
+      scale_alpha_continuous(range = c(0.2, 1), breaks = seq(0, 1, 0.1)) +
+      ggtitle(paste("Shot Heatmap for", input$player)) +
+      ylim(0, 40) +
+      xlim(-25, 25)
+  })
+}
 
-DOESNT RENDER RIGHT&(#&((&!)$&(* H DHUYWIQC NWNvls,v)))!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!uio vgrsiouv
-
-
-plot<-dateCondensed%>% 
-  ggplot(aes(x=GAME_DATE,y=Three_Pts_Made_Total,color=PLAYER_NAME))+
-  geom_line(alpha=0.8)+
-  theme_solarized_2(light=F)+
-  labs(title="NBA Total Three Pointers Race, 2022-2023 Season",
-       caption="Source: Dom Samangy, Github, NBA_Shots_04_23")+
-  theme(text=element_text(colour="#EEEEEE"),
-        title=element_text(colour="#EEEEEE",size=9,face = "bold"),
-        plot.title=element_text(hjust=0.5),
-        axis.title.x = element_blank(),
-        panel.grid.minor.x=element_blank(),
-        legend.background = element_blank(),
-        legend.key= element_blank(),
-        legend.position=c(0.095, 0.81), ## legend at top-left, inside the plot
-        plot.margin = unit(c(0.5,1.3,0.5,0.5), "cm"))+ 
-  scale_x_date(date_labels = "%b",date_breaks ="1 month")
-
-
-plot.animation=plot+
-  transition_reveal(GAME_DATE)+
-  view_follow(fixed_y=T)
-
-#Save animation
-animate(plot.animation, height=365,width=608,fps=30,duration=10, renderer = gifski_renderer())
-
-anim_save(plot.animation, filename = "NBA_Total_Three_Pointers_Race_2022-2023_Season.gif")
+shinyApp(ui, server)
 
 
